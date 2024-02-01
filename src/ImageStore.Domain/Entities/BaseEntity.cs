fc7ddc532cdb65377bcdcd213f850dd1359573ef
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-02 00:22:36 +0200

    Setup basic domain objects

---
+++ src/ImageStore.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageStore.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/ImageStore.Domain/Entities/PostRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageStore.Domain.Entities
+{
+    public class PostRequest : BaseEntity
+    {
+        public PostRequestData Data { get; set; }
+        public string Status { get; set; }
+        public string? FailureDetails { get; set; }
+    }
+
+    public class PostRequestData
+    {
+        public string Caption { get; set; }
+        public string Image { get; set; }
+        public string Creator { get; set; }
+    }
+}
