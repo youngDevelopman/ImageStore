c53e058457a840d9521c763ec4a8ba8b345b0a2e
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-03 01:15:10 +0200

    Use dependecy injection to register infrastructure

--- src/ImageStore.Infrastructure/DependencyInjection.cs
+++ src/ImageStore.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,7 @@
-﻿using ImageStore.Infrastructure.Database;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ImageStore.Domain.Interfaces;
-using ImageStore.Infrastructure.ImageStore;
 
 namespace ImageStore.Infrastructure
 {
@@ -16,7 +13,7 @@ namespace ImageStore.Infrastructure
             services.AddDbContext<ApplicationDbContext>((sp, options) =>
             {
                 // TODO: Use interceptor to save createdat and updatedat fields
-                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                //options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
                 options.UseSqlServer(connectionString);
             });
             services.AddScoped<IImageStorage, S3ImageStore>();
