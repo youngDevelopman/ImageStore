9dec39d0b8898cc85167a3cf1399d53dc94dab9d
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-03 00:46:28 +0200

    Create baarebones of Infrastructure project

---
+++ src/ImageStore.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ImageStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+namespace ImageStore.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<PostRequest> PostRequests { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> ops) : base(ops)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
