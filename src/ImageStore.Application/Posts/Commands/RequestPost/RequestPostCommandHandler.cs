591f5611e7d2a08f548d5bef82f98b19f803fe0d
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-02 22:25:23 +0200

    Add RequestPostCommand

---
+++ src/ImageStore.Application/Posts/Commands/RequestPost/RequestPostCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using ImageStore.Domain.Entities;
+using ImageStore.Domain.Interfaces;
+using MediatR;
+
+namespace ImageStore.Application.Posts.Commands.RequestPost
+{
+    public class RequestPostCommandHandler : IRequestHandler<RequestPostCommand>
+    {
+        private readonly IImageStorage _imageStorage;
+        private readonly IPostRepository _postRepository;
+        public RequestPostCommandHandler(IImageStorage imageStorage, IPostRepository postRepository)
+        {
+            _imageStorage = imageStorage;
+            _postRepository = postRepository;
+        }
+        // TODO: Complete the implementation
+        public async Task Handle(RequestPostCommand request, CancellationToken cancellationToken)
+        {
+            await _imageStorage.UploadFileAsync(request.file, cancellationToken);
+
+            // TODO: Add other fields
+            var postRequest = new PostRequest()
+            {
+               Data = new PostRequestData()
+               {
+                   Caption = request.content,
+               }
+            };
+            await _postRepository.AddPostRequestAsync(postRequest, cancellationToken);
+        }
+    }
+}
