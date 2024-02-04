7ff716acefe4d2b44319310b92b6cea86831dd6f
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-04 20:16:10 +0200

    Call CreatePostCommand from SQS image processor when message has been received

---
+++ src/ImageStore.Application/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using ImageStore.Domain.Interfaces;
+using MediatR;
+
+namespace ImageStore.Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand>
+    {
+        private readonly IImageStorage _imageStorage;
+        public CreatePostCommandHandler(IImageStorage imageStorage)
+        {
+            _imageStorage = imageStorage;
+        }
+
+        public Task Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("CreatePostCommandHandler");
+            return Task.CompletedTask;
+        }
+    }
+}
