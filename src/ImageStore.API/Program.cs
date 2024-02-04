071cc32515326b180106af7d39cca36c2de29e80
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-04 16:42:25 +0200

    Construct AWS services manually. Add SQS Image processor

--- src/ImageStore.API/Program.cs
+++ src/ImageStore.API/Program.cs
@@ -1,4 +1,7 @@
+using Amazon.S3;
+using ImageStore.API.Services;
 using ImageStore.Infrastructure;
+using ImageStore.Infrastructure.Configuration;
 namespace ImageStore.API
 {
     public class Program
@@ -9,7 +12,14 @@ namespace ImageStore.API
 
             // Add services to the container.
             builder.Services.AddApplicationDependencies();
-            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+            var awsCredentialsConfuguration = GetAwsCredentialsConfuguration(builder.Configuration);
+            builder.Services.AddSingleton<AwsCredentialsConfuguration>(x => awsCredentialsConfuguration);
+
+            builder.Services.AddInfrastructureServices(builder.Configuration, awsCredentialsConfuguration);
+
+            builder.Services.AddHostedService<SQSProcessedImageProcessor>();
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -30,6 +40,14 @@ namespace ImageStore.API
             app.MapControllers();
 
             app.Run();
+        }
+
+        public static AwsCredentialsConfuguration GetAwsCredentialsConfuguration(IConfiguration configuration)
+        {
+            var accessKey = configuration["AWS:AccessKey"];
+            var secretKey = configuration["AWS:SecretKey"];
+            var region = configuration["AWS:Region"];
+            return new AwsCredentialsConfuguration(accessKey, secretKey, region);
         }
     }
 }
---
+++ src/ImageStore.Infrastructure/Configuration/AwsCredentialsConfuguration.cs
@@ -0,0 +1,18 @@
+﻿using Amazon;
+using Amazon.Runtime;
+
+namespace ImageStore.Infrastructure.Configuration
+{
+    public class AwsCredentialsConfuguration
+    {
+        public BasicAWSCredentials Credentials { get; }
+        public RegionEndpoint Region { get; }
+
+        public AwsCredentialsConfuguration(string accessKey, string secretKey, string region)
+        {
+            // No need to check for null values because it is handled internally
+            Credentials = new BasicAWSCredentials(accessKey, secretKey); 
+            Region = Amazon.RegionEndpoint.GetBySystemName(region);
+        }
+    }
+}
