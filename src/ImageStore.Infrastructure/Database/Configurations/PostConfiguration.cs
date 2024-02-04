534bb4d2284ea5ffa90bcca101dbab66167d3f3d
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-04 22:06:50 +0200

    Add Post Table

---
+++ src/ImageStore.Infrastructure/Database/Configurations/PostConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ImageStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImageStore.Infrastructure.Database.Configurations
+{
+    internal class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // We probaby do not want to have captions that have too many characters
+            builder.Property(x => x.Caption)
+                .IsRequired(false)
+                .HasMaxLength(100);
+        }
+    }
+}
