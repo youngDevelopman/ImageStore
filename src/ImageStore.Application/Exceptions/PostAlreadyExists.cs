d3d5bd3ac5d843d0b351b5a0e5d634ce73781fcd
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-04 22:21:00 +0200

    Handle the sutiation where post already exists

---
+++ src/ImageStore.Application/Exceptions/PostAlreadyExists.cs
@@ -0,0 +1,19 @@
+﻿namespace ImageStore.Application.Exceptions
+{
+    public class PostAlreadyExists : Exception
+    {
+        public PostAlreadyExists()
+        {
+        }
+
+        public PostAlreadyExists(string message)
+            : base(message)
+        {
+        }
+
+        public PostAlreadyExists(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- src/ImageStore.Application/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
+++ src/ImageStore.Application/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -1,20 +1,59 @@
-﻿using ImageStore.Domain.Interfaces;
+﻿using ImageStore.Domain.Entities;
+using ImageStore.Domain.Interfaces;
+using ImageStore.Domain.Enums;
 using MediatR;
+using ImageStore.Application.Exceptions;
 
 namespace ImageStore.Application.Posts.Commands.CreatePost
 {
     public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand>
     {
-        private readonly IImageStorage _imageStorage;
-        public CreatePostCommandHandler(IImageStorage imageStorage)
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPostRepository _postRepository;
+        public CreatePostCommandHandler(IPostRepository postRepository, IUnitOfWork unitOfWork)
         {
-            _imageStorage = imageStorage;
+            _unitOfWork = unitOfWork;
+            _postRepository = postRepository;
         }
 
-        public Task Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        public async Task Handle(CreatePostCommand request, CancellationToken cancellationToken)
         {
+            await using var transaction = await _unitOfWork.BeginTransactionAsync();
+
+            // TODO: Move whole logic to repository method
+            try
+            {
+                var postRequest = await _postRepository.GetPostRequestByIdAsync(request.requestId, cancellationToken);
+                if(postRequest == null) 
+                { 
+                    // TODO: Throw Exception
+                }
+                else if (postRequest.Status == PostRequestStatus.PostCreated)
+                {
+                    throw new PostAlreadyExists($"Post already exists. Post Id: {postRequest.PostId}");
+                }
+
+                var post = new Post()
+                {
+                    Caption = postRequest.Data.Caption,
+                    Creator = postRequest.Data.Creator,
+                    Image = request.ImagePath.ToString(),
+                };
+
+                await _postRepository.AddPostAsync(post, cancellationToken);
+                postRequest.Status = PostRequestStatus.PostCreated;
+
+                await _unitOfWork.CommitAsync();
+
+                await transaction.CommitAsync();
+            }
+            catch(Exception ex)
+            {
+                //TODO: Log the exception
+                await transaction.RollbackAsync();
+                throw;
+            }
             Console.WriteLine("CreatePostCommandHandler");
-            return Task.CompletedTask;
         }
     }
 }
