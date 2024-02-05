e4720c30e8f4cb6427238de8b124d3fe361ea1e1
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-05 21:41:26 +0200

    Add comments table and configuration

---
+++ src/ImageStore.Infrastructure/Database/Configurations/CommentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using ImageStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImageStore.Infrastructure.Database.Configurations
+{
+    internal class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            // We probaby do not want to have comments that have too many characters
+            builder.Property(x => x.Content)
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ src/ImageStore.Infrastructure/Database/Migrations/20240205193222_AddCommentsTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageStore.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CommentsCount",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Version",
+                table: "Posts",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentsCount",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Posts");
+        }
+    }
+}
