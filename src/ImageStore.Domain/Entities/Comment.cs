410a873f700b0c837ae21f3021508ac8a995498b
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-05 21:42:01 +0200

    Modify comment and post entitites

--- src/ImageStore.Domain/Entities/Comment.cs
+++ src/ImageStore.Domain/Entities/Comment.cs
@@ -2,6 +2,8 @@
 {
     public class Comment : BaseEntity
     {
+        public Guid PostId { get; set; } 
+        public string User { get; set; }
         public string Content { get; set; }
     }
 }
