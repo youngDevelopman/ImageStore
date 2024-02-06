c93b20bab6f07517e5ddfefc3ba79128f22122b1
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-06 02:14:05 +0200

    Add validator

---
+++ src/ImageStore.Application/Comments/Commands/AddComment/AddCommentCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ImageStore.Application.Comments.Commands.AddComment
+{
+    public class AddCommentCommandValidator : AbstractValidator<AddCommentCommand>
+    {
+        public AddCommentCommandValidator()
+        {
+            RuleFor(x => x.Content)
+                .MaximumLength(100);
+        }
+    }
+}
