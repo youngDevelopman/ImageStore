d9ac8d3a62bcfbbf7fe93ba931905103851f46d9
Nazar Mazuryk <nazar.mazuryk@angloamerican.com>
2024-02-06 16:51:07 +0200

    Add Users table migration

---
+++ src/ImageStore.Infrastructure/Database/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using ImageStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImageStore.Infrastructure.Database.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.UserName)
+                 .HasMaxLength(20)
+                 .IsRequired();
+        }
+    }
+}
