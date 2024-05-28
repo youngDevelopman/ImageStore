1d35425c181b49823db244777a53e087f83f5303
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:06 +0300

    Add registration flow

--- src/ImageStore.Application/DependencyInjection.cs
+++ src/ImageStore.Application/DependencyInjection.cs
@@ -3,13 +3,22 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using FluentValidation;
+using ImageStore.Application.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace ImageStore.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<JwtConfiguration>(x => 
+            {
+                var jwtConfigurationSection = configuration.GetRequiredSection("Jwt");
+                x.Issuer = jwtConfigurationSection.GetRequiredSection(nameof(JwtConfiguration.Issuer)).Value;
+                x.Key = jwtConfigurationSection.GetRequiredSection(nameof(JwtConfiguration.Key)).Value;
+            });
+
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             services.AddMediatR(config => {
