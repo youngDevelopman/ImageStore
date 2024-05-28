12588a3ba8c051644914440470c7fec27daa88d9
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:31:50 +0300

    Call CreatePostCommand from SQS image processor when message has been received

--- src/ImageStore.API/Services/SQSProcessedImageProcessor.cs
+++ src/ImageStore.API/Services/SQSProcessedImageProcessor.cs
@@ -1,16 +1,27 @@
 ﻿using Amazon.S3;
+using Amazon.S3.Util;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using ImageStore.API.Exceptions;
+using ImageStore.Application.Posts.Commands.CreatePost;
 using ImageStore.Infrastructure.Configuration;
+using MediatR;
 
 namespace ImageStore.API.Services
 {
     public class SQSProcessedImageProcessor : BackgroundService
     {
         private readonly IAmazonSQS _queue;
-        public SQSProcessedImageProcessor(AwsCredentialsConfuguration awsCredentialsConfuguration)
+        private readonly IServiceProvider _services;
+        private readonly ILogger<SQSProcessedImageProcessor> _logger;
+        public SQSProcessedImageProcessor(
+            AwsCredentialsConfuguration awsCredentialsConfuguration, 
+            IServiceProvider services, 
+            ILogger<SQSProcessedImageProcessor> logger)
         {
             _queue = new AmazonSQSClient(awsCredentialsConfuguration.Credentials, awsCredentialsConfuguration.Region);
+            _services = services;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -20,19 +31,59 @@ namespace ImageStore.API.Services
                 Console.WriteLine($"getting messages from the queue {DateTime.Now}");
                 var request = new ReceiveMessageRequest()
                 {
-                    QueueUrl = "https://sqs.eu-central-1.amazonaws.com/648524383514/ImageStore-ProcessedImages-Queue",
+                    QueueUrl = "https://sqs.eu-central-1.amazonaws.com/648524383514/ImageStore-ProcessedImages-Queue", //TODO: retrieve from config
                     MaxNumberOfMessages = 3,
                     WaitTimeSeconds = 5, // This parameter enables long polling for SQS
                 };
 
                 var response = await _queue.ReceiveMessageAsync(request);
-                foreach (var message in response.Messages)
+                if (response.Messages.Any()) // Posibility of getting 0 messages
                 {
-                    Console.WriteLine(message.Body);
-                    if (message.Body.Contains("Exception")) continue;
+                    try
+                    {
+                        using var scope = _services.CreateScope();
+                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                        var s3Client = scope.ServiceProvider.GetRequiredService<IAmazonS3>();
+                        foreach (var message in response.Messages)
+                        {
+                            var s3Event = S3EventNotification.ParseJson(message.Body);
+                            var records = s3Event.Records;
+                            foreach (var record in records)
+                            {
+                                var filePath = record.S3.Object.Key;
+                                var bucket = record.S3.Bucket.Name;
+                                var region = record.AwsRegion;
 
-                    //await _queue.DeleteMessageAsync(
-                        //"https://sqs.eu-central-1.amazonaws.com/648524383514/ImageStore-ProcessedImages-Queue", message.ReceiptHandle);
+                                var imageUrl = new Uri($"https://{bucket}.s3.{region}.amazonaws.com/{filePath}");
+
+                                var fileMetadata = await s3Client.GetObjectMetadataAsync(bucket, filePath);
+
+                                var requestIdStr = fileMetadata.Metadata["post-request-id"];
+                                if(requestIdStr == null )
+                                {
+                                    throw new RequestIdNotFound($"Request Id not found for {imageUrl}");
+                                }
+                                var requestId = Guid.Parse(requestIdStr);
+
+                                await mediator.Send(new CreatePostCommand(requestId, imageUrl));
+                            }
+                            Console.WriteLine(message.Body);
+                            if (message.Body.Contains("Exception")) continue;
+
+                            //await _queue.DeleteMessageAsync(
+                            //"https://sqs.eu-central-1.amazonaws.com/648524383514/ImageStore-ProcessedImages-Queue", message.ReceiptHandle);
+                        }
+                    }
+                    catch (RequestIdNotFound ex)
+                    {
+                        // TODO: Put message to DLQ and remove from queue
+                        _logger.LogError(ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($" Unknown error {ex.Message}");
+                    }
+
                 }
             }
         }
