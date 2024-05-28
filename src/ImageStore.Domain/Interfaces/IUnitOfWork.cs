d6027e26492871aa6da601fac19ad52b70899e1f
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:34:15 +0300

    Add Transaction support for unit of work class

--- src/ImageStore.Domain/Interfaces/IUnitOfWork.cs
+++ src/ImageStore.Domain/Interfaces/IUnitOfWork.cs
@@ -1,7 +1,11 @@
-﻿namespace ImageStore.Domain.Interfaces
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ImageStore.Domain.Interfaces
 {
     public interface IUnitOfWork
     {
+        Task<IDbContextTransaction> BeginTransactionAsync();
         Task<int> CommitAsync(CancellationToken cancellationToken = default(CancellationToken));
+
     }
 }
