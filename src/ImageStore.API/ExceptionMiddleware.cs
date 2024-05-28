1be474ccf1d9af4697134cc5ce2bc223df3c9796
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:00 +0300

    Add global exception middleware

---
+++ src/ImageStore.API/ExceptionMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    private readonly Dictionary<Type, Func<HttpContext, Exception, Task>> _exceptionHandlers;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _logger = logger;
+        _next = next;
+        _exceptionHandlers = new()
+            {
+                { typeof(ValidationException), HandleValidationException },
+                { typeof(Exception), HandleExceptionAsync },
+            };
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+
+            var exceptionType = ex.GetType();
+
+            if (_exceptionHandlers.ContainsKey(exceptionType))
+            {
+                await _exceptionHandlers[exceptionType].Invoke(httpContext, ex);
+            }
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        await context.Response.WriteAsync(new ProblemDetails()
+        {
+            Status = context.Response.StatusCode,
+            Detail = "Internal Server Error."
+        }.ToString());
+    }
+
+    private async Task HandleValidationException(HttpContext httpContext, Exception ex)
+    {
+        var exception = (ValidationException)ex;
+
+        httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        await httpContext.Response.WriteAsJsonAsync(new ValidationProblemDetails(exception.Errors)
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        });
+    }
+}
