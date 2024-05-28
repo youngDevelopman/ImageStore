efad71b488afbd55474e76591c66926624a80007
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:31:46 +0300

    Create baarebones of Infrastructure project

---
+++ src/ImageStore.Infrastructure/Database/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using ImageStore.Domain.Interfaces;
+
+namespace ImageStore.Infrastructure.Database
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CommitAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/ImageStore.Infrastructure/ImageStore/S3ImageStore.cs
@@ -0,0 +1,12 @@
+﻿using ImageStore.Domain.Interfaces;
+
+namespace ImageStore.Infrastructure.ImageStore
+{
+    public class S3ImageStore : IImageStorage
+    {
+        public Task UploadFileAsync(Stream fileStream, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
