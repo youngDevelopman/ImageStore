add60b292dc6be37eb7040bfad1c9b16f0d4c241
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:58 +0300

    Create baarebones of Infrastructure project

---
+++ src/ImageStore.Infrastructure/Database/Repositories/PostRepository.cs
@@ -0,0 +1,18 @@
+﻿using ImageStore.Domain.Entities;
+using ImageStore.Domain.Interfaces;
+
+namespace ImageStore.Infrastructure.Database.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;   
+        }
+        public async Task AddPostRequestAsync(PostRequest postRequest, CancellationToken cancellationToken)
+        {
+           await _context.PostRequests.AddAsync(postRequest, cancellationToken);
+        }
+    }
+}
