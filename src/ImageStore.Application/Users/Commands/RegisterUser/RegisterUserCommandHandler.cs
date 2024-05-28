e71aed1612edcad717359b2226c841048e7345f3
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:04 +0300

    Implement user registration flow

---
+++ src/ImageStore.Application/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using ImageStore.Application.Exceptions;
+using ImageStore.Application.Utils;
+using ImageStore.Domain.Entities;
+using ImageStore.Domain.Interfaces;
+using MediatR;
+
+namespace ImageStore.Application.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserResultDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public RegisterUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<RegisterUserResultDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(request.Email, cancellationToken);
+            if (user != null)
+            {
+                throw new UserAlreadyExists($"User with email {user.Email} already exists");
+            }
+
+            (string salt, string hash) = PasswordHasher.HashPassword(request.Password);
+            var newUser = new User()
+            {
+                Email = request.Email,
+                UserName = request.UserName,
+                PasswordHash = hash,
+                PasswordSalt = salt,
+            };
+
+            await _userRepository.AddUserAdync(newUser, cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return new RegisterUserResultDto(newUser.Id, newUser.Email, newUser.UserName);
+        }
+    }
+}
