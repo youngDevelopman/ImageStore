5dab0a0e81039f81bc840b49f7d3a6c92378d4ca
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:56 +0300

    Add base interfaces for the domain

---
+++ src/ImageStore.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageStore.Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task AddPostRequestAsync(PostRequest postRequest, CancellationToken cancellationToken);
+    }
+}
