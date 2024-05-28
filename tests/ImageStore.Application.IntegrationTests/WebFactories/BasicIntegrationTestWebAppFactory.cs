88a75bfd425eb11c039c22597829ca5a9cc759c0
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:33:32 +0300

    Add unit tests project Add GetPaginatedPosts_PassPageSizeOnly_ShouldReturnExpectedPosts test case

---
+++ tests/ImageStore.Application.IntegrationTests/WebFactories/BasicIntegrationTestWebAppFactory.cs
@@ -0,0 +1,58 @@
+﻿using ImageStore.API;
+using ImageStore.Domain.Entities;
+using ImageStore.Infrastructure.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Org.BouncyCastle.Pqc.Crypto.Lms;
+using Testcontainers.MsSql;
+
+/// <summary>
+/// Basic Integration test factory that configures MS SQL Docker image without Seed
+/// </summary>
+public class BasicIntegrationTestWebAppFactory
+    : WebApplicationFactory<Program>,
+      IAsyncLifetime
+{
+    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptorType =
+                typeof(DbContextOptions<ApplicationDbContext>);
+
+            var descriptor = services
+                .SingleOrDefault(s => s.ServiceType == descriptorType);
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(_dbContainer.GetConnectionString()));
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        using var scope = Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
+        Seed(dbContext);
+    }
+
+    public virtual void Seed(ApplicationDbContext context) { }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+}
