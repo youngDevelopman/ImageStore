7251625d76661bdbb9d2eb57fb8a827ec4e22930
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:34:44 +0300

    Add comments for the PostPaginationQueryBuilder

--- src/ImageStore.Infrastructure/Database/PostPaginationQueryBuilder.cs
+++ src/ImageStore.Infrastructure/Database/PostPaginationQueryBuilder.cs
@@ -2,6 +2,9 @@
 
 namespace ImageStore.Infrastructure.Database
 {
+    /// <summary>
+    /// This class is used for counstructing post pagination query 
+    /// </summary>
     public class PostPaginationQueryBuilder
     {
         private IQueryable<Post> _queryResult;
@@ -15,6 +18,11 @@ namespace ImageStore.Infrastructure.Database
             _queryResult = context.Posts.AsQueryable();
         }
 
+        /// <summary>
+        /// Set the pagination strategy. Remember, if set to PreviousCount the query that was built would return the previous set of data in wrong order. Make sure to reverse it back.
+        /// </summary>
+        /// <param name="paginationStrategy"> </param>
+        /// <returns></returns>
         public PostPaginationQueryBuilder SetPaginationStrategy(PaginationStrategy paginationStrategy)
         {
             _paginationStrategy = paginationStrategy;
@@ -41,8 +49,26 @@ namespace ImageStore.Infrastructure.Database
 
         public IQueryable<Post> Build()
         {
+            /*
+                The requirement is to take the list of posts sorted by the number of comments using the cursor based pagination.
+
+                Cursor-based pagination requires sorting by the unique key to generate cursor, otherwise we would not get consistent results
+                So, the both id and comment count fields need to be included. 
+                Updated at was also added to define the cursor, because we would have an ability to obtain the latest posts
+            */
+
             if(_paginationStrategy == PaginationStrategy.PreviousPage)
             {
+                /* In case PreviousPage pagination strategy we want to retrieve the previous 
+                  set of data starting from cursor that is provided. 
+                
+                For that we are going to do a simple trick - reverse the order of the ORDER BY fields
+                    and reverse the filteing conditions (i.e. change less than to greater than)
+
+                But there is one thing to remember 
+                    - the returned data is sorted in reverse, so we need to apply an additional ordering 
+                    to bring the data back to the desired order.
+                */
                 _queryResult = _queryResult
                     .OrderBy(post => post.CommentsCount)
                     .ThenBy(post => post.UpdatedAt)
@@ -54,11 +80,18 @@ namespace ImageStore.Infrastructure.Database
             }
             else
             {
+                // Set a base ordering query
                 _queryResult = _queryResult
                     .OrderByDescending(post => post.CommentsCount)
                     .ThenByDescending(post => post.UpdatedAt)
                     .ThenByDescending(post => post.Id);
 
+                /* To retrieve next page we need to provide cursor and add the filtering logic 
+                    which helps us to determine where the starting post item is located
+
+                    if pagination strategy is FirstPage, there is no need to apply the filtering below, 
+                        because we always want to take the x amount of posts right from the start
+                 * */
                 if(_paginationStrategy == PaginationStrategy.NextPage)
                 {
                     _queryResult = _queryResult.Where(post =>
@@ -68,15 +101,18 @@ namespace ImageStore.Infrastructure.Database
                 }
             }
 
+            // Include the comments
             if (_includeComments)
             {
                 _queryResult = _queryResult
                     .Include(post => post.Comments);
             }
 
+            // Take the _size amount of records
             _queryResult = _queryResult
                 .Take(_size);
 
+            // Take x number of the latest comments of the post
             if (_includeComments)
             {
                 _queryResult = _queryResult
