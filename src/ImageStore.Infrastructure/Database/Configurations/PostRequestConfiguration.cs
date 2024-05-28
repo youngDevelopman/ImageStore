458956f9948701c1708223e369178261b73a48d4
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:32:40 +0300

    Create baarebones of Infrastructure project

---
+++ src/ImageStore.Infrastructure/Database/Configurations/PostRequestConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ImageStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImageStore.Infrastructure.Database.Configurations
+{
+    public class PostRequestConfiguration : IEntityTypeConfiguration<PostRequest>
+    {
+        public void Configure(EntityTypeBuilder<PostRequest> builder)
+        {
+            builder.OwnsOne(x => x.Data, ownedNavigationBuilder =>
+            {
+                ownedNavigationBuilder.ToJson();
+            });
+        }
+    }
+}
---
+++ src/ImageStore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using ImageStore.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ImageStore.Domain.Interfaces;
+using ImageStore.Infrastructure.ImageStore;
+
+namespace ImageStore.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                // TODO: Use interceptor to save createdat and updatedat fields
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IImageStorage, S3ImageStore>();
+            return services;
+        }
+    }
+}
