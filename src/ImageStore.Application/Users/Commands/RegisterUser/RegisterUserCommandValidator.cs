e0e93bbebfa428f8466932ab4957d1de75bb4130
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:31:59 +0300

    Add validation for user registration

---
+++ src/ImageStore.Application/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace ImageStore.Application.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommandValidator: AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible);
+
+            RuleFor(x => x.Password)
+                .MinimumLength(5);
+
+            RuleFor(x => x.UserName)
+                .MinimumLength(5);
+        }
+    }
+}
