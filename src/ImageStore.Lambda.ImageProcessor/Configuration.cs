ce4e11d6dd5c9741bdfe3df0257dea86d52051a4
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:32:46 +0300

    Add AWS Lambda for image processing

---
+++ src/ImageStore.Lambda.ImageProcessor/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace ImageStore.Lambda.ImageProcessor
+{
+    public class Configuration
+    {
+        public string BucketName { get; set; }
+        public const string BucketNameKey = "Bucket_Name";
+
+        public string OutputFolder { get; set; }
+        public const string OutputFolderKey = "Output_Folder";
+
+        public int ImageHeight { get; set; }
+        public const string ImageHeightKey = "Image_Height";
+
+        public int ImageWidth { get; set; }
+        public const string ImageWidthKey = "Image_Width";
+
+        public string ImageFormat { get; set; }
+        public const string ImageFormatKey = "Image_Format";
+    }
+}
