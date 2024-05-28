45c07929fae10a1774a0dc909954acd402b8ea26
Nazar Mazuryk <nazarmazurik99@gmail.com>
2024-05-28 15:31:48 +0300

    Add controller method to store post requests in the Database

--- src/ImageStore.API/Program.cs
+++ src/ImageStore.API/Program.cs
@@ -8,6 +8,7 @@ namespace ImageStore.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddApplicationDependencies();
             builder.Services.AddInfrastructureServices(builder.Configuration);
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
